77584fa94a15c4bf43f26dba31e0001330b119c0
h4sitha <hasithamania123@gmail.com>
2023-12-28 21:56:45 +0530

    Add functions to run when checkboxes are clicked

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ generateButton.addEventListener("click", generatePasswords);
 function generatePasswords() {
     console.log("Button Clicked!");
 }
+
+
+let simpleLetters = document.getElementById("s-letters-box");
+let capitalLetters = document.getElementById("c-letters-box");
+let numbersList = document.getElementById("numbers-box");
+let symbolsList = document.getElementById("symbols-box");
+
+simpleLetters.addEventListener("click", addSimpleLetters);
+capitalLetters.addEventListener("click", addCapitalLetters);
+numbersList.addEventListener("click", addNumbers);
+symbolsList.addEventListener("click", addSymbols);
+
+
+function addSimpleLetters() {
+    console.log("Simple Letters Checkbox")
+}
+
+function addCapitalLetters() {
+    console.log("Capital Letters Checkbox")
+}
+
+function addNumbers() {
+    console.log("Numbers Checkbox")
+}
+
+function addSymbols() {
+    console.log("Symbols Checkbox")
+}
