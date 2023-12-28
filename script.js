9f970b3169df1d480f0919b5edb1ecea053154cf
h4sitha <hasithamania123@gmail.com>
2023-12-28 21:47:29 +0530

    Add a function to run when the button is clicked

---
+++ script.js
@@ -0,0 +1,7 @@
+let generateButton = document.querySelector("button");
+
+generateButton.addEventListener("click", generatePasswords);
+
+function generatePasswords() {
+    console.log("Button Clicked!");
+}
